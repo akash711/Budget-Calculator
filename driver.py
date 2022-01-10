adb322ef5f8f3a03ecd228f29228a78f7701f44f
akash_prame <57642848+akash711@users.noreply.github.com>
2022-01-10 23:30:53 +0100

    Added methods to wallet class

--- driver.py
+++ driver.py
@@ -1,25 +1,34 @@
 # This entrypoint file to be used in development. Start by reading README.md
-import budget
-from budget import Category, create_spend_chart
+#import budget
+#from budget import Category, create_spend_chart
 from unittest import main
+from wallet import *
 
-food = budget.Category("Food")
-food.deposit(1000, "initial deposit")
-food.withdraw(10.15, "groceries")
-food.withdraw(15.89, "restaurant and more food for dessert")
-print(food.get_balance())
-clothing = budget.Category("Clothing")
-food.transfer(50, clothing)
-clothing.withdraw(25.55)
-clothing.withdraw(100)
-auto = budget.Category("Auto")
-auto.deposit(1000, "initial deposit")
-auto.withdraw(15)
 
-print(food)
-print(clothing)
+# food = budget.Category("Food")
+# food.deposit(1000, "initial deposit")
+# food.withdraw(10.15, "groceries")
+# food.withdraw(15.89, "restaurant and more food for dessert")
+# print(food.get_balance())
+# clothing = budget.Category("Clothing")
+# food.transfer(50, clothing)
+# clothing.withdraw(25.55)
+# clothing.withdraw(100)
+# auto = budget.Category("Auto")
+# auto.deposit(1000, "initial deposit")
+# auto.withdraw(15)
 
-print(create_spend_chart([food, clothing, auto]))
+# print(food)
+# print(clothing)
 
-# Run unit tests automatically
-main(module='test_module', exit=False)
+# print(create_spend_chart([food, clothing, auto]))
+
+# # Run unit tests automatically
+# main(module='test_module', exit=False)
+
+wallet = Wallet('Akash')
+wallet.read_from_excel('myWallet.csv')
+print(wallet.ledger)
+print(wallet.categories)
+
+
--- wallet.py
+++ wallet.py
@@ -1,48 +1,142 @@
+import pandas as pd
+
 class Wallet:
     
+    all_wallets = []
+
     def __init__(self, name: str):
+
         print("Wallet: '{}' created!".format(name))
         self.name = name
         self.ledger = []
         self.balance = 0
+        self.categories = {}
+        
+        #Register for all wallets created
+        Wallet.all_wallets.append(self)
     
-    def deposit(self, amt: float):
+    def deposit(self, amt: float, category:str):
         
         if amt >= 0:
             self.balance = self.balance + amt
         else: 
             raise Exception('Amount cannot be negative!')
+        
+        if category in self.categories.keys():
+            self.categories[category] += amt
+        else:
+            self.categories[category] = amt 
+        
+        self.ledger.append('{} deposited'.format(amt))
+        
+        
+        
 
     def withdraw(self, amt: float):
+        """
+        Withdraw method:
+            -Withdraw money from account 
+        """
         
         if amt >= 0:
             self.balance = self.balance - amt
         else:
             raise Exception('Amount cannot be negative!')
 
+        if "Withdraw" in self.categories.keys():
+            self.categories["Withdraw"] -= amt
+        else:
+            self.categories["Withdraw"] = -(amt) 
 
-    def spend(self, amt: float, description: str):
-        
+        self.ledger.append('{} withdrawn'.format(amt))
+
+    
+    
+    def spend(self, amt: float, category: str, description: str):
+        """
+        Spend method:
+            -Use money on specific category
+        """
+
         if amt >= 0:
             self.balance = self.balance - amt
         else:
             raise Exception('Amount cannot be negative!')
         
+        if category in self.categories.keys():
+            self.categories[category] -= amt 
+        else:
+            self.categories[category] = -(amt)
         
-        print("New transaction: {}, Amount: {}".format(description, amt))
-        
+        self.ledger.append('{} spent on {}: {}'.format(amt, category, description))
+
 
 
     def get_balance(self):
         print('Available Balance: {}'.format(self.balance))
 
 
-    def send_money(self, amt, recepient):
 
+    def transfer(self, amt: float, recepient: str):
+        """
+        Transfer money to recepient
+        """
+        
         if amt >= 0:
             self.balance = self.balance - amt
         else:
             raise Exception('Amount cannot be negative!')
+
+        if "Transfer" in self.categories.keys():
+            self.categories["Transfer"] -= amt 
+        else:
+            self.categories["Transfer"] = -(amt)
+
+        self.ledger.append('{} transferred to {}'.format(amt, recepient))
+
+    def receive(self, amt: float, sender: str):
+        """
+        Receive money from sender
+        """
         
-        print("{} sent to {}".format(amt, recepient))
+        if amt >= 0:
+            self.balance = self.balance + amt
+        else:
+            raise Exception('Amount cannot be negative!')
+
+        if "Receive" in self.categories.keys():
+            self.categories["Receive"] += amt 
+        else:
+            self.categories["Receive"] = amt
+
+        self.ledger.append('{} received from {}'.format(amt, sender))
+
         
+
+    def read_from_excel(self, csv:str):
+        
+        data = pd.read_csv(csv)
+        
+        for i in range(len(data)):
+            if data['Type'][i] == 'D':
+                self.deposit(amt = data['Amount'][i], category=data['Category'][i])
+            elif data['Type'][i] == 'T':
+                self.transfer(amt = data['Amount'][i], recepient=data['Description'][i])
+            elif data['Type'][i] == 'W':
+                self.withdraw(amt = data['Amount'][i])
+            elif data['Type'][i] == 'S':
+                self.spend(amt = data['Amount'][i], category=data['Category'][i], description=data['Description'][i])
+            else:
+                raise Exception('Invalid Transaction Type!')
+            
+    
+    def print_ledger(self):
+        
+        for i in self.ledger:
+            print(i)
+
+
+
+    def __repr__(self):
+        return "Wallet({})".format(self.name)
+
